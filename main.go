7d55daf844ea70d71163bf707c8779e03423206a
Awang <me@karisma.id>
2026-01-23 07:10:51 +0000

    feat: implement category endpoint

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -17,9 +18,51 @@ type Produk struct {
 	Jumlah int    `json:"jumlah"`
 }
 
+type Kategori struct {
+	ID        string `json:"id"`
+	Nama      string `json:"nama"`
+	Deskripsi string `json:"deskripsi"`
+}
+
 var semuaProduk = []Produk{
 	{ID: "b83628e5-6bad-4d7d-868e-32a9fe8ad84f", Nama: "Apple iPhone 17 ProMax", Harga: 27000000, Jumlah: 10},
 	{ID: "3e1ca5e5-a3cb-4705-9311-456f1f263d96", Nama: "Nokia 3310", Harga: 100000, Jumlah: 5},
+}
+
+var semuaKategori = []Kategori{
+	{ID: "1518d979-1590-475d-84b3-54c9423f1686", Nama: "Mobile", Deskripsi: "Produk mobile"},
+	{ID: "8bdff33e-bf7f-451d-a244-e2311761cb26", Nama: "Laptop", Deskripsi: "Produk laptop"},
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		lrw := &loggingResponseWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		log.Printf("[REQUEST] %s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		next.ServeHTTP(lrw, r)
+		duration := time.Since(start)
+		log.Printf("[RESPONSE] %s %s %s %d %s",
+			r.RemoteAddr,
+			r.Method,
+			r.URL.Path,
+			lrw.statusCode,
+			duration,
+		)
+	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
 
 func handleResponse(w http.ResponseWriter, statusCode int, data interface{}) {
@@ -45,12 +88,10 @@ func getHealthz(w http.ResponseWriter, r *http.Request) {
 }
 
 func getProduk(w http.ResponseWriter, r *http.Request) {
-	log.Println("GET produk")
 	handleResponse(w, http.StatusOK, semuaProduk)
 }
 
 func createProduk(w http.ResponseWriter, r *http.Request) {
-	log.Println("POST Produk")
 	var produk Produk
 	err := json.NewDecoder(r.Body).Decode(&produk)
 	if err != nil {
@@ -70,7 +111,6 @@ func createProduk(w http.ResponseWriter, r *http.Request) {
 }
 
 func getProdukByID(w http.ResponseWriter, r *http.Request) {
-	log.Println("GET produk by id")
 	id, err := uuid.Parse(mux.Vars(r)["id"])
 	if err != nil {
 		log.Println(err)
@@ -87,7 +127,6 @@ func getProdukByID(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateProdukByID(w http.ResponseWriter, r *http.Request) {
-	log.Println("PUT produk")
 	id, err := uuid.Parse(mux.Vars(r)["id"])
 	if err != nil {
 		log.Println(err)
@@ -116,8 +155,6 @@ func updateProdukByID(w http.ResponseWriter, r *http.Request) {
 
 func deleteProdukByID(w http.ResponseWriter, r *http.Request) {
 	id, err := uuid.Parse(mux.Vars(r)["id"])
-	log.Printf("DELETE produk by id %s", id.String())
-
 	if err != nil {
 		log.Println(err)
 		handleError(w, http.StatusBadRequest, "Invalid uuid")
@@ -134,19 +171,107 @@ func deleteProdukByID(w http.ResponseWriter, r *http.Request) {
 	handleError(w, http.StatusNotFound, "Product not found")
 }
 
+func getKategori(w http.ResponseWriter, r *http.Request) {
+	handleResponse(w, http.StatusOK, semuaKategori)
+}
+
+func getKategoriByID(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		log.Println(err)
+		handleError(w, http.StatusBadRequest, "Invalid uuid")
+		return
+	}
+	for _, kategori := range semuaKategori {
+		if kategori.ID == id.String() {
+			handleResponse(w, http.StatusOK, kategori)
+			return
+		}
+	}
+	handleError(w, http.StatusNotFound, "Kategori not found")
+}
+
+func createKategori(w http.ResponseWriter, r *http.Request) {
+	var kategori Kategori
+	err := json.NewDecoder(r.Body).Decode(&kategori)
+	if err != nil {
+		log.Println(err)
+		handleError(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+	id, err := uuid.NewUUID()
+	if err != nil {
+		log.Println(err)
+		handleError(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+	kategori.ID = id.String()
+	semuaKategori = append(semuaKategori, kategori)
+	handleResponse(w, http.StatusCreated, kategori)
+}
+
+func updateKategoriByID(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		log.Println(err)
+		handleError(w, http.StatusBadRequest, "Invalid uuid")
+		return
+	}
+	var kategoriBaru Kategori
+	err = json.NewDecoder(r.Body).Decode(&kategoriBaru)
+	if err != nil {
+		log.Println(err)
+		handleError(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+	for i, kategori := range semuaKategori {
+		if kategori.ID == id.String() {
+			kategori.Nama = kategoriBaru.Nama
+			kategori.Deskripsi = kategoriBaru.Deskripsi
+			semuaKategori[i] = kategori
+			handleResponse(w, http.StatusOK, kategori)
+			return
+		}
+	}
+	handleError(w, http.StatusNotFound, "Kategori not found")
+}
+
+func deleteKategoriByID(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(mux.Vars(r)["id"])
+	if err != nil {
+		log.Println(err)
+		handleError(w, http.StatusBadRequest, "Invalid uuid")
+		return
+	}
+	log.Println(id)
+	for i, kategori := range semuaKategori {
+		if kategori.ID == id.String() {
+			semuaKategori = append(semuaKategori[:i], semuaKategori[i+1:]...)
+			handleResponse(w, http.StatusOK, kategori)
+			return
+		}
+	}
+	handleError(w, http.StatusNotFound, "Kategori not found")
+}
+
 func main() {
 	log.Println("Starting server...")
 	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
+
 	r.HandleFunc("/healthz", getHealthz).Methods("GET")
+
 	r.HandleFunc("/api/produk", getProduk).Methods("GET")
 	r.HandleFunc("/api/produk", createProduk).Methods("POST")
 	r.HandleFunc("/api/produk/{id}", getProdukByID).Methods("GET")
 	r.HandleFunc("/api/produk/{id}", updateProdukByID).Methods("PUT")
 	r.HandleFunc("/api/produk/{id}", deleteProdukByID).Methods("DELETE")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
-	})
+	r.HandleFunc("/api/kategori", getKategori).Methods("GET")
+	r.HandleFunc("/api/kategori", createKategori).Methods("POST")
+	r.HandleFunc("/api/kategori/{id}", getKategoriByID).Methods("GET")
+	r.HandleFunc("/api/kategori/{id}", updateKategoriByID).Methods("PUT")
+	r.HandleFunc("/api/kategori/{id}", deleteKategoriByID).Methods("DELETE")
 
 	log.Println("Listening to port 8080")
 	err := http.ListenAndServe(":8080", r)
